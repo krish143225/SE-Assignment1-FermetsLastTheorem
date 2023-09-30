9502b768045bbf5710d9893b9298d1011289f2cc
krish143225 <134121307+krish143225@users.noreply.github.com>
2023-09-30 04:03:30 -0500

    Delete FermatsLastTheorem/src/Fermats/fermatsmain.java

--- FermatsLastTheorem/src/Fermats/fermatsmain.java
+++
@@ -1,8 +0,0 @@
-package Fermats;
-
-public class fermatsmain {
-    public static void main(String[] args) {
-	FermatsTheorem fermatsTheorem = new FermatsTheorem();
-    	fermatsTheorem.process();
-    }
-}
